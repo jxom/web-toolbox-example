1c61df09002ab0f1dc6f2dc0564f97726716d0dc
jxom <jakemoxey@gmail.com>
2017-10-17 09:44:20 +1100

    changes

--- packages/react/src/Button/Button.js
+++ packages/react/src/Button/Button.js
@@ -1,21 +1,30 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import '@jmoxey/styleguide/button.css';
+import '../../styleguide/button.css';
 
 class Button extends React.Component {
   render() {
+    const { disabled, onClick, ...props } = this.props;
     return (
-      <button className="button">Cool too</button>
-    )
+      <button className={disabled ? 'is-loading' : ''} onClick={!disabled ? onClick : () => {}} {...props}>
+        {
+          disabled ?
+          <span>Loading...</span> :
+          <span>Get Medipass health status</span>
+        }
+      </button>
+    );
   }
 }
 
 Button.propTypes = {
-
+  disabled: PropTypes.bool,
+  onClick: PropTypes.func
 };
 
 Button.defaultProps = {
-
+  disabled: false,
+  onClick: () => {}
 };
 
 export default Button;
